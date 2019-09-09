c367b98837376510fe8ced9741015678412dd73c
jakegalligan <galligan1996@gmail.com>
2019-09-09 15:57:49 -0400

    Seting up initial API call to retrieve a list of teams from API and storing them in variable called Teams within the TeamList component

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TeamList from './TeamList';
+
+const App = () => {
+  return (
+    <div>
+      <TeamList />
+    </div>
+  )
+}
+
+
+export default App;
